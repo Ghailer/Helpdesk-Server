a050131c00e5825e4248dc087bee5fe4ca34059a
Ghailer <ghailer@hotmail.com>
2017-04-14 13:53:09 +0200

    Update ClientSocket.cs

--- Helpdesk Server/ClientSocket.cs
+++ Helpdesk Server/ClientSocket.cs
@@ -10,7 +10,24 @@ namespace Helpdesk_Server
     public class ClientSocket
     {
         TcpClient client;
-        string user = "x";       
+        
+        
+        public void testowo(Dictionary<string, Action<string>> testDict)
+        {
+            // testDict.Add("INSERT INTO Users" , ((string x) => AddUser(command)));
+            testDict.Add("INSERT INTO Users", ((string x) => Console.WriteLine("String1")));
+            testDict.Add("test2", ((string x) => Console.WriteLine("String2")));
+        }
+
+        public void testrun(Dictionary<string, Action<string>> testDict)
+        {
+            if (testDict.ContainsKey("test2"))
+            {
+                Console.WriteLine("test2");
+                var funkcja = testDict["test2"];
+                funkcja.Invoke("sss");
+            }
+        }
         
         public ClientSocket(TcpClient Client)
         {
